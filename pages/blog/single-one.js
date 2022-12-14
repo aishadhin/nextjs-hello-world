21bbe89c0b645718da870a1056fb96a740f57e76
Ai Shadhin <shadhinahmed80@gmail.com>
2022-12-14 09:40:17 +0600

    nested route added

---
+++ pages/blog/single-one.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const singleone = () => {
+    return (
+        <div>
+            <h2>Single blog-1</h2>
+        </div>
+    );
+};
+
+export default singleone;
---
+++ pages/blog/single-two.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const singletwo = () => {
+  return (
+    <div>
+      <h2>Single blog-2</h2>
+    </div>
+  );
+};
+
+export default singletwo;
